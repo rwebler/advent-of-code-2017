56705a4da43014af08a62bd573edb71dde6fedd5
Rodrigo <rwebler@gmail.com>
2017-12-01 12:04:25 -0200

    Day 1 2/2

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ import (
 func main() {
 	fmt.Println("Advent of Code 2017 in Go")
 	args := os.Args[1:]
-	if len(args) > 0 {
-		fmt.Printf("Reverse Captcha for %s is %d\n", args[0], ReverseCaptcha(args[0]))
+	if len(args) > 1 {
+		day, _ := strconv.Atoi(args[0])
+		puzzle, _ := strconv.Atoi(args[1])
+		star := day*2 + puzzle - 2
+		switch star {
+		case 1:
+			fmt.Printf("Reverse Captcha for %s is %d\n", args[2], ReverseCaptcha(args[2]))
+		case 2:
+			fmt.Printf("Reverse Captcha for %s is %d\n", args[2], ReverseCaptchaHalfway(args[2]))
+		default:
+			fmt.Println("Not done yet.")
+		}
 	} else {
-		fmt.Println("Please provide an input for the Captcha")
+		fmt.Println("Please provide the correct input (day, 1/2, args)")
 	}
 }
 
@@ -27,6 +37,24 @@ func ReverseCaptcha(input string) int {
 		} else {
 			ii, _ = strconv.Atoi(string(input[p+1]))
 		}
+		if i == ii {
+			sum += i
+		}
+	}
+	return sum
+}
+
+//ReverseCaptcha returns the sum of halfway repeat integers in a circular sequence
+func ReverseCaptchaHalfway(input string) int {
+	sum := 0
+	for p, c := range input {
+		i, _ := strconv.Atoi(string(c))
+		var ii int
+		p2 := p + len(input)/2
+		if p >= (len(input) / 2) {
+			p2 = p2 - len(input)
+		}
+		ii, _ = strconv.Atoi(string(input[p2]))
 		if i == ii {
 			sum += i
 		}
--- main_test.go
+++ main_test.go
@@ -22,3 +22,33 @@ func TestReverseCaptcha(t *testing.T) {
 		t.Errorf("Expected output of 9 but got %d", o4)
 	}
 }
+
+func TestReverseCaptchaHalfway(t *testing.T) {
+	/*
+		1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+		1221 produces 0, because every comparison is between a 1 and a 2.
+		123425 produces 4, because both 2s match each other, but no other digit has a match.
+		123123 produces 12.
+		12131415 produces 4.
+	*/
+	o1 := ReverseCaptchaHalfway("1212")
+	if o1 != 6 {
+		t.Errorf("Expected output of 6 but got %d", o1)
+	}
+	o2 := ReverseCaptchaHalfway("1221")
+	if o2 != 0 {
+		t.Errorf("Expected output of 0 but got %d", o2)
+	}
+	o3 := ReverseCaptchaHalfway("123425")
+	if o3 != 4 {
+		t.Errorf("Expected output of 4 but got %d", o3)
+	}
+	o4 := ReverseCaptchaHalfway("123123")
+	if o4 != 12 {
+		t.Errorf("Expected output of 12 but got %d", o4)
+	}
+	o5 := ReverseCaptchaHalfway("12131415")
+	if o5 != 4 {
+		t.Errorf("Expected output of 4 but got %d", o5)
+	}
+}
